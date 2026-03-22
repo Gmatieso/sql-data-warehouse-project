acc344ce0f623d8689e74a61c2ed61e6dd996064
Gmatieso <matiesogeoffrey@gmail.com>
2026-03-22 13:17:59 +0300

    Chore: Refactor our silve table

---
+++ scripts/create_tbl_silver.sql
@@ -0,0 +1,84 @@
+-- Drop a table called 'silver.crm_cust_info' in schema 'dbo'
+-- Drop the table if it already exists
+IF OBJECT_ID('[silver].[crm_cust_info]', 'U') IS NOT NULL
+DROP TABLE [silver].[crm_cust_info]
+GO
+CREATE TABLE silver.crm_cust_info(
+cst_id INT,
+cst_key NVARCHAR(50),
+cst_firstname NVARCHAR(50),
+cst_lastname NVARCHAR(50),
+cst_marital_status NVARCHAR(50),
+cst_gndr NVARCHAR(50),
+cst_create_date DATE
+);
+
+
+-- Drop a table called 'silver.crm_prd_info' in schema 'dbo'
+-- Drop the table if it already exists
+IF OBJECT_ID('[silver].[crm_prd_info]', 'U') IS NOT NULL
+DROP TABLE [silver].[crm_prd_info]
+GO
+CREATE TABLE silver.crm_prd_info(
+prd_id INT,
+prd_key NVARCHAR(50),
+prd_name NVARCHAR(50),
+prd_cost INT,
+prd_line NVARCHAR(50),
+prd_start_dt DATETIME,
+prd_end_dt DATETIME
+);
+
+-- Drop a table called 'silver.crm_sales_details' in schema 'dbo'
+-- Drop the table if it already exists
+IF OBJECT_ID('[silver].[crm_sales_details]', 'U') IS NOT NULL
+DROP TABLE [silver].[crm_sales_details]
+GO
+CREATE TABLE silver.crm_sales_details(
+sls_ord_num NVARCHAR(50),
+sls_prd_key NVARCHAR(50),
+sls_cust_id INT,
+sls_order_dt INT,
+sls_ship_dt INT,
+sls_due_dt INT,
+sls_sales INT,
+sls_quantity INT,
+sls_price INT
+)
+
+-- Drop a table called 'silver.erp_cust_az12' in schema 'dbo'
+-- Drop the table if it already exists
+IF OBJECT_ID('[silver].[erp_cust_az12]', 'U') IS NOT NULL
+DROP TABLE [silver].[erp_cust_az12]
+GO
+
+CREATE TABLE silver.erp_cust_az12(
+cid NVARCHAR(50),
+bdate DATETIME,
+gen NVARCHAR(50)
+)
+
+-- Drop a table called 'silver.erp_loc_a101' in schema 'dbo'
+-- Drop the table if it already exists
+IF OBJECT_ID('[silver].[erp_loc_a101]', 'U') IS NOT NULL
+DROP TABLE [silver].[erp_loc_a101]
+GO
+
+CREATE TABLE silver.erp_loc_a101(
+cid NVARCHAR(50),
+cntry NVARCHAR(50)
+)
+
+
+-- Drop a table called 'silver.erp_px_cat_g1v2' in schema 'dbo'
+-- Drop the table if it already exists
+IF OBJECT_ID('[silver].[erp_px_cat_g1v2]', 'U') IS NOT NULL
+DROP TABLE [silver].[erp_px_cat_g1v2]
+GO
+
+CREATE TABLE silver.erp_px_cat_g1v2(
+id NVARCHAR(50),
+cat NVARCHAR(50),
+subcat NVARCHAR(50),
+maintenance NVARCHAR(50)
+)
